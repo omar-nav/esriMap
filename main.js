24c41d352ea3c4a4603ad643dffd92c2bfaf5b43
omar_navarro77 <omar.navarro@traviscountytx.gov>
2020-03-30 18:05:56 -0500

    TODO obtain search result for sending out a subsequent HTTP request

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ require(["esri/Map", "esri/views/MapView", "esri/widgets/Search"], function(
   Search
 ) {
   var map = new Map({
-    basemap: "topo-vector"
+    basemap: "streets-vector"
   });
 
   var view = new MapView({
     container: "viewDiv",
     map: map,
-    center: [-118.805, 34.027], // longitude, latitude
+    center: [-97.745363, 30.270545], // longitude, latitude
     zoom: 13
   });
 
@@ -18,4 +18,8 @@ require(["esri/Map", "esri/views/MapView", "esri/widgets/Search"], function(
     view: view
   });
   view.ui.add(search, "top-right");
+
+  // console.log(view.ui.watch(search.SearchResult));
+  //
+  // obtain search result address
 });
